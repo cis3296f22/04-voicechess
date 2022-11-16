e779e8e4a7df5adb26dcd8e849c40198858fe547
NathanAdiam <nathan.adiam@temple.edu>
2022-11-16 18:27:35 -0500

    Refactored variable names

--- src/test.py
+++ src/test.py
@@ -4,7 +4,8 @@ from typing import List
 import piece
 import square
 
-def test_inital_pawn_moves():
+
+def test_initial_pawn_moves():
     # Set up board
     board: List['square.Square']
     position = [
@@ -30,13 +31,13 @@ def test_inital_pawn_moves():
                 )
 
     # Set up piece to move
-    p = piece.Piece(color=piece.PieceColor.Black,piece_type=piece.PieceType.Pawn,position=(1,1))
+    moving_piece = piece.Piece(color=piece.PieceColor.Black, piece_type=piece.PieceType.Pawn, position=(1, 1))
 
     # initially pawn moves either one square forward or two squares forward
-    expectedAnswer = [(1,2) , (1,3)]
+    expected_answer = [(1, 2), (1, 3)]
 
     # Check possible moves for black pawn located at (1,1)
-    assert p.possible_moves(board) == expectedAnswer
+    assert moving_piece.possible_moves(board) == expected_answer
 
 
 def test_pawn_can_eat_move():
@@ -65,13 +66,14 @@ def test_pawn_can_eat_move():
                 )
 
     # Set up piece to move
-    p = piece.Piece(color=piece.PieceColor.White, piece_type=piece.PieceType.Pawn, position=(1, 4))
+    moving_piece = piece.Piece(color=piece.PieceColor.White, piece_type=piece.PieceType.Pawn, position=(1, 4))
 
     # White pawn eats black pawn diagonally and can also move forward
-    expectedAnswer = [(1, 3),(2, 3)]
+    expected_answer = [(1, 3), (2, 3)]
 
     # Check possible moves for white pawn located at (1,4)
-    assert set(p.possible_moves(board)) == set(expectedAnswer)
+    assert set(moving_piece.possible_moves(board)) == set(expected_answer)
+
 
 def test_rook_moves():
     # Set up board
@@ -99,15 +101,15 @@ def test_rook_moves():
                 )
 
     # Set up piece to move
-    p = piece.Piece(color=piece.PieceColor.White, piece_type=piece.PieceType.Rook, position=(7, 7))
+    moving_piece = piece.Piece(color=piece.PieceColor.White, piece_type=piece.PieceType.Rook, position=(7, 7))
 
     # White rook moves either vertically or horizontally until it faces an obstacle
     # can_eat (7, 1)
     # can_move (7, 6), (7, 5), (7, 4), (7, 3), (7, 2)
-    expectedAnswer = [(7, 6), (7, 5), (7, 4), (7, 3), (7, 2), (7, 1)]
+    expected_answer = [(7, 6), (7, 5), (7, 4), (7, 3), (7, 2), (7, 1)]
 
     # Check possible moves for white rook located at (7,7)
-    assert set(p.possible_moves(board)) == set(expectedAnswer)
+    assert set(moving_piece.possible_moves(board)) == set(expected_answer)
 
 
 def test_knight_moves():
@@ -136,15 +138,16 @@ def test_knight_moves():
                 )
 
     # Set up piece to move
-    p = piece.Piece(color=piece.PieceColor.White, piece_type=piece.PieceType.Knight, position=(6, 4))
+    moving_piece = piece.Piece(color=piece.PieceColor.White, piece_type=piece.PieceType.Knight, position=(6, 4))
 
     # White knight moves in an L-shape
     # can_eat (5, 2)
     # can_move (7, 2), (4, 3), (4, 5), (7, 6)
-    expectedAnswer = [(5, 2), (7, 2), (4, 3), (4, 5), (7, 6)]
+    expected_answer = [(5, 2), (7, 2), (4, 3), (4, 5), (7, 6)]
 
     # Check possible moves for white knight located at (6,4)
-    assert set(p.possible_moves(board)) == set(expectedAnswer)
+    assert set(moving_piece.possible_moves(board)) == set(expected_answer)
+
 
 def test_bishop_moves():
     # Set up board
@@ -172,15 +175,16 @@ def test_bishop_moves():
                 )
 
     # Set up piece to move
-    p = piece.Piece(color=piece.PieceColor.Black, piece_type=piece.PieceType.Bishop, position=(5, 3))
+    moving_piece = piece.Piece(color=piece.PieceColor.Black, piece_type=piece.PieceType.Bishop, position=(5, 3))
 
     # Black bishop moves diagonally
     # can_eat (6, 4), (2, 6)
     # can_move (2,0), (3, 1), (4, 2), (6, 2), (4, 4), (3, 5), (2, 6)
-    expectedAnswer = [(6, 4), (2, 6), (2,0), (3, 1), (4, 2), (6, 2), (4, 4), (3, 5), (2, 6)]
+    expected_answer = [(6, 4), (2, 6), (2, 0), (3, 1), (4, 2), (6, 2), (4, 4), (3, 5), (2, 6)]
 
     # Check possible moves for black bishop located at (5,3)
-    assert set(p.possible_moves(board)) == set(expectedAnswer)
+    assert set(moving_piece.possible_moves(board)) == set(expected_answer)
+
 
 def test_king_moves():
     # Set up board
@@ -208,15 +212,16 @@ def test_king_moves():
                 )
 
     # Set up piece to move
-    p = piece.Piece(color=piece.PieceColor.White, piece_type=piece.PieceType.King, position=(4, 5))
+    moving_piece = piece.Piece(color=piece.PieceColor.White, piece_type=piece.PieceType.King, position=(4, 5))
 
     # Black bishop moves diagonally
     # can_eat
     # can_move (5, 5), (3, 5), (5, 6), (5, 3), (4, 4)
-    expectedAnswer = [(3, 4), (3, 5), (4, 6), (5, 5), (5, 4)]
+    expected_answer = [(3, 4), (3, 5), (4, 6), (5, 5), (5, 4)]
 
     # Check possible moves for white king located at (4,5)
-    assert set(p.possible_moves(board)) == set(expectedAnswer)
+    assert set(moving_piece.possible_moves(board)) == set(expected_answer)
+
 
 def test_queen_moves():
     # Set up board
@@ -244,15 +249,16 @@ def test_queen_moves():
                 )
 
     # Set up piece to move
-    p = piece.Piece(color=piece.PieceColor.Black, piece_type=piece.PieceType.Queen, position=(3, 3))
+    moving_piece = piece.Piece(color=piece.PieceColor.Black, piece_type=piece.PieceType.Queen, position=(3, 3))
 
     # Black queen moves diagonally + vertically + horizontally
     # can_eat (4, 4), (0, 6), (3, 6)
     # can_move (5, 1), (2, 2), (4, 2), (4, 3), (2, 4), (3, 4), (1, 5), (3, 5)
-    expectedAnswer = [(4, 4), (0, 6), (3, 6), (5, 1), (2, 2), (4, 2), (4, 3), (2, 4), (3, 4), (1, 5), (3, 5)]
+    expected_answer = [(4, 4), (0, 6), (3, 6), (5, 1), (2, 2), (4, 2), (4, 3), (2, 4), (3, 4), (1, 5), (3, 5)]
 
     # Check possible moves for black queen located at (3,3)
-    assert set(p.possible_moves(board)) == set(expectedAnswer)
+    assert set(moving_piece.possible_moves(board)) == set(expected_answer)
+
 
 if __name__ == '__main__':
     unittest.main()
